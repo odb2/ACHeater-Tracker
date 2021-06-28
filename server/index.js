c0148272b33ada7b55a1022cc003a3e40ea8ebb6
Oscar <odbarragan99@gmail.com>
2021-06-27 18:15:51 -0700

    a

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 // All other GET requests not handled before will return our React app
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
