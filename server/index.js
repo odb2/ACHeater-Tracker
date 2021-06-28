53afb9d0f8e3a88ceb105be28f0ca2b5bfab65b2
Oscar <odbarragan99@gmail.com>
2021-06-27 18:09:27 -0700

    test

--- server/index.js
+++ server/index.js
@@ -5,22 +5,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+// All other GET requests not handled before will return our React app
 
-    app.use(express.static(path.join(__dirname, "/../../client/build")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "/../../client/build", "index.html"));
-    });
-  }
-else{
-    app.use(express.static('client/build'));
-    // All other GET requests not handled before will return our React app
-    app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build', 'index.html'));
-    });
-  }
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
